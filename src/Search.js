0b07ead7cefdc56588e8022302ba876ecf83992e
Liamlhy <liamlilhy@gmail.com>
2019-10-06 16:32:35 -0500

    finish tutorial

--- src/Search.js
+++ src/Search.js
@@ -1,53 +1,58 @@
 import React, { Component } from "react";
-import './Search.css'
+import "./Search.css";
+
 class Search extends Component {
-state = {
-  searchValue: '',
-  meal: []
-};
-handleOnChange = event => {
-  this.setState({searchValue: event.target.value });
-};
-handleSearch = () => {
-  this.makeApiCall(this.state.searchValue);
+  state = {
+    searchValue: "",
+    meals: []
+  };
+
+  handleOnChange = event => {
+    this.setState({ searchValue: event.target.value });
+  };
+
+  handleSearch = () => {
+    this.makeApiCall(this.state.searchValue);
+  };
+
+  makeApiCall = searchInput => {
+    var searchUrl = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchInput}`;
+    fetch(searchUrl)
+      .then(response => {
+        return response.json();
+      })
+      .then(jsonData => {
+        this.setState({ meals: jsonData.meals });
+      });
+  };
+
+  render() {
+    return (
+      <div id="main">
+        <h1>Welcome to the meal search app</h1>
+        <input
+          name="text"
+          type="text"
+          placeholder="Search"
+          onChange={event => this.handleOnChange(event)}
+          value={this.state.searchValue}
+        />
+        <button onClick={this.handleSearch}>Search</button>
+        {this.state.meals ? (
+          <div id="meals-container">
+            {this.state.meals.map((meal, index) => (
+              <div class="single-meal" key={index}>
+                <h2>{meal.strMeal}</h2>
+                <img src={meal.strMealThumb} alt="meal-thumbnail" />
+              </div>
+            ))}
+          </div>
+        ) : (
+          <p>Try searching for a meal</p>
+        )}
+      </div>
+    );
+  }
 }
-makeApiCall = searchInput =>{
-  var searchUrl = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchInput}`;
-  fetch(searchUrl)
-  .then(response => {
-    return response.json();
-  })
-  .then(jsonData => {
-    console.log(jsonData.meals);
-    this.setState({meals: jsonData.meals });
-  });
-};
-render() {
-  return (
-  <div>
-  <h1>Welcome to the meal search app</h1>
-  <input
-  name="text"
-  type="text"
-  placeholder="Search"
-  onChange={event => this.handleOnChange(event)}
-  value = {this.state.searchValue}
-  />
-  <button onClick={this.handleOnChange}>Search</button>
-  {this.state.meals ? (
-  <div id = "meals-container">
-  {this.state.meals.map((meal, index) => (
-  <div class = "single-meal" key={index}>
-  <h2>{meal.strMeal}</h2>
-  <img src={meal.strMealThumb} alt="meal-thumbnail" />
-  </div>
-  ))}
-  </div>
-  ) : (
-  <p>Try searching for a meal</p>
-  )}
-  </div>
-  );
-}
-}
+
 export default Search;
